c13a0e64883d71b639d94b38c23b5ab37c31f02d
Karl Spies <Karl.Spies@gmx.net>
2012-03-31 13:38:26 +0200

    Initial commit

---
+++ controllers/IndexContoller.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: spies
+ * Date: 31.03.12 (13 KW)
+ * Time: 13:32
+ * To change this template use File | Settings | File Templates.
+ */
+class Hackathon_Logger_IndexContoller
+{
+
+}
