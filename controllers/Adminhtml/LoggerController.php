011d2c4a31bf940cf293c8e3eb9d872adad9483a
Karl Spies <Karl.Spies@gmx.net>
2012-04-01 01:58:14 +0200

    Refactor controllers to be really admin controllers

--- controllers/Adminhtml/LoggerController.php
+++ controllers/Adminhtml/LoggerController.php
@@ -1,13 +1,12 @@
 <?php
-class Hackathon_Logger_ViewController extends Mage_Adminhtml_Controller_Action
+class Hackathon_Logger_Adminhtml_LoggerController extends Mage_Adminhtml_Controller_Action
 {
 
     public function indexAction()
     {
 		$collection = Mage::getModel('hackathon_logger/db_entry')->getCollection();
 		foreach($collection as $entry) {
-			echo $entry->getMessage().PHP_EOL;
+			echo $entry->getMessage().PHP_EOL.'<br />';
 		}
-		$v = 0;
     }
 }
