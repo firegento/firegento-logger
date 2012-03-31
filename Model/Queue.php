0ff77765c9ac9f8e11e026435d116916a91a06ec
Karl Spies <Karl.Spies@gmx.net>
2012-03-31 21:59:11 +0200

    Prepared all loggers to use queued logger

---
+++ Model/Queue.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: spies
+ * Date: 31.03.12 (13 KW)
+ * Time: 20:58
+ * To change this template use File | Settings | File Templates.
+ */
+class Hackathon_Logger_Model_Queue extends Zend_Log_Writer_Abstract
+{
+	/** @var Zend_Log_Writer_Abstract */
+	private $_logger_mock = null;
+
+	private $_logger_cache = array();
+
+	public function __construct($filename)
+	{
+		/** @var $helper Hackathon_Logger_Helper_Data */
+		$helper = Mage::helper('hackathon_logger');
+		$target = $helper->getLoggerConfig('targets/targets_value');
+		/** @var $converter Hackathon_Logger_Model_System_Config_Source_Targets */
+		$converter = Mage::getModel('hackathon_logger/system_config_source_targets');
+		$className = $converter->optionToClass($target);
+
+		$this->_logger_mock = new $className($filename);
+	}
+
+	/**
+	 * Write a message to the log.
+	 *
+	 * @param  array  $event  log data event
+	 * @return void
+	 */
+	protected function _write($event)
+	{
+		//we write first to the internal array
+		$this->_logger_cache[] = $event;
+	}
+	/**
+	 * At the end of the request we write to the actual logger
+	 */
+	public function __destruct()
+	{
+		foreach($this->_logger_cache as $event){
+			$this->_logger_mock->_write($event);
+		}
+	}
+
+	/**
+	 * Construct a Zend_Log driver
+	 *
+	 * @param  array|Zend_Config $config
+	 * @return Zend_Log_FactoryInterface
+	 */
+	static public function factory($config)
+	{
+		// TODO: Implement factory() method.
+	}
+}
--- Model/System/Config/Source/Targets.php
+++ Model/System/Config/Source/Targets.php
@@ -6,15 +6,53 @@
  * Time: 15:36
  * To change this template use File | Settings | File Templates.
  */
-class Hackathon_Logger_Model_System_Config_Source_Targets
+class Hackathon_Logger_Model_System_Config_Source_Targets extends Varien_Object
 {
+	private $_options = array();
+
+	const ZEND_LOG_WRITER_STREAM = "Zend_Log_Writer_Stream";
+
+	public function _construct()
+	{
+		$helper = Mage::helper('hackathon_logger');
+		$this->_options[] = array('label' => $helper->__('Mail'), 'value' => 'mail', 'class' => 'Hackathon_Logger_Model_Mail');
+		$this->_options[] = array('label' => $helper->__('DB'), 'value' => 'db', 'class' => 'Zend_Log_Writer_Db');
+		$this->_options[] = array('label' => $helper->__('XMPP'), 'value' => 'xmpp', 'class' => 'Hackathon_Logger_Model_Mail');
+		$this->_options[] = array('label' => $helper->__('File'), 'value' => 'file', 'class' => self::ZEND_LOG_WRITER_STREAM);
+		$this->_options[] = array('label' => $helper->__('Dropbox'), 'value' => 'dropbox', 'class' => 'Hackathon_Logger_Model_Dropbox');
+	}
+	/**
+	 * @return array
+	 */
 	public function toOptionArray()
-    {
-        $helper = Mage::helper('hackathon_logger');
-        return array(
-            array('label' => $helper->__('Mail'), 'value' => 'mail'),
-            array('label' => $helper->__('DB'), 'value' => 'db'),
-            array('label' => $helper->__('XMPP'), 'value' => 'xmpp'),
-        );
-    }
+	{
+		return $this->_options;
+	}
+	/**
+	 * Get the actual Class for the stored Backend value
+	 * @param $optionValue
+	 * @return string
+	 */
+	public function optionToClass($optionValue)
+	{
+		$current = self::ZEND_LOG_WRITER_STREAM;
+		foreach ($this->_options as $option) {
+			if ($option['value'] == $optionValue) {
+				$current = $option['class'];
+			}
+		}
+		return $current;
+	}
+	/**
+	 * Set the actual options array. Should be in the format array('label' => '', 'value' => '', 'class' => '')
+	 * @param $options
+	 */
+	public function setOptions($options)
+	{
+		$this->_options = $options;
+	}
+
+	public function addOption($label,$value, $className) {
+		$this->_options[] = array('label' => $label, 'value' => $value, 'class' => $className);
+	}
 }
