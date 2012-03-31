81a86c46d193a75e36aa4f7d5f1504e34b3666ad
Karl Spies <Karl.Spies@gmx.net>
2012-04-01 01:42:45 +0200

    Setup Resource Model

---
+++ Model/Db/Entry.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: spies
+ * Date: 01.04.12 (14 KW)
+ * Time: 01:22
+ * To change this template use File | Settings | File Templates.
+ */
+class Hackathon_Logger_Model_Db_Entry extends Mage_Core_Model_Abstract
+{
+	public function  _construct()
+	{
+		$this->_init('hackathon_logger/db_entry');
+	}
+
+}
--- controllers/ViewController.php
+++ controllers/ViewController.php
@@ -4,6 +4,10 @@ class Hackathon_Logger_ViewController extends Mage_Adminhtml_Controller_Action
 
     public function indexAction()
     {
-
+		$collection = Mage::getModel('hackathon_logger/db_entry')->getCollection();
+		foreach($collection as $entry) {
+			echo $entry->getMessage().PHP_EOL;
+		}
+		$v = 0;
     }
 }
