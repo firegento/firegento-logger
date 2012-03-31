caf041924737746be9b53a61a6cdf1a996497f33
Karl Spies <Karl.Spies@gmx.net>
2012-03-31 14:37:38 +0200

    Working Mail Logger, allmost.

--- Model/Mail.php
+++ Model/Mail.php
@@ -8,9 +8,43 @@
  */
 class Hackathon_Logger_Model_Mail extends Zend_Log_Writer_Mail
 {
+	private $transport = null;
 
 	public function __construct($filename) {
-		$mail = new Zend_Mail();
-		parent::__construct($mail);
+		parent::__construct(null);
+	}
+
+	public function _write($event){
+		//Lazy intatiation of underlying mailer
+		if($this->_mail === null) {
+			$this->_mail = $this->getMail();
+		}
+		parent::_write($event);
+		/*
+		$this->getMail()->setSubject('TestBetreff');
+		$this->getMail()->setBodyText('Das ist der Text des Mails.');
+		$this->getMail()->send($this->transport);
+		*/
+	}
+
+	public function getMail(){
+		if($this->_mail === null) {
+			//TODO: Read Config from backend or database
+			$this->_mail = new Zend_Mail();
+			$this->_mail->setFrom('hackathon@icyapp.de', 'Einige Sender');
+			$this->_mail->addTo('Karl.Spies@gmx.net', 'Einige Empfänger');
+		}
+		return $this->_mail;
+	}
+
+	public function getTransport() {
+		if($this->transport === null) {
+			//TODO: Read Config from backend or database
+			$config = array('auth' => 'login',
+							'username' => 'm02439fd',
+							'password' => 'hackathon2012');
+			$this->transport = new Zend_Mail_Transport_Smtp('icyapp.de', $config);
+		}
+		return $this->transport;
 	}
 }
