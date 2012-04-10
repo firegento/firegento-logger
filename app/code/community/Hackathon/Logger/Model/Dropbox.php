04bad44b8b6a13ac45d80eaeb868277fb50bf544
Damian Luszczymak <info@icyapp.de>
2012-04-10 14:28:14 +0300

    stupid daim ;-)

--- app/code/community/Hackathon/Logger/Model/Dropbox.php
+++ app/code/community/Hackathon/Logger/Model/Dropbox.php
@@ -6,15 +6,15 @@ class Hackathon_Logger_Model_Dropbox extends Zend_Log_Writer_Abstract
     public function __construct($filename)
     {
         // Set your consumer key, secret and callback URL
-        $key      = 'ykheuj6if9aodkn';
-        $secret   = 'cof0elsd3t8d9k0';
+        $key      = '???';
+        $secret   = '????';
 
         // Check whether to use HTTPS and set the callback URL
         $protocol = (!empty($_SERVER['HTTPS'])) ? 'https' : 'http';
         $callback = $protocol . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 
         // Instantiate the required Dropbox objects
-        $encrypter = new \Dropbox\OAuth\Storage\Encrypter('cof0elsd3t8d9k0');
+        $encrypter = new \Dropbox\OAuth\Storage\Encrypter('????');
         $storage = new \Dropbox\OAuth\Storage\Session($encrypter);
         $OAuth = new \Dropbox\OAuth\Consumer\Curl($key, $secret, $storage, $callback);
         $dropbox = new \Dropbox\API($OAuth);
