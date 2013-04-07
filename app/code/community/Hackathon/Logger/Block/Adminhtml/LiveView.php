ce5e10df3558fc06a776ce154bf655050de91951
Marco Becker <marco.becker@flagbit.de>
2013-04-07 02:42:51 +0200

    [~TASK] modified JavaScript and add Controls to select a Log-File

--- app/code/community/Hackathon/Logger/Block/Adminhtml/LiveView.php
+++ app/code/community/Hackathon/Logger/Block/Adminhtml/LiveView.php
@@ -1,4 +1,24 @@
 <?php
-class Hackathon_Logger_Block_Adminhtml_LiveView extends Mage_Core_Block_Template {
+
+class Hackathon_Logger_Block_Adminhtml_LiveView extends Mage_Core_Block_Template
+{
+
+    public function getLogFiles()
+    {
+        $logFiles = array();
+
+        $path = Mage::getBaseDir('var');
+        $logPath = $path . '/log';
+
+        $d = dir($logPath);
+        while (false !== ($entry = $d->read())) {
+            if ( strstr($entry, '.log') ) {
+                $logFiles[] = $entry;
+            }
+        }
+        $d->close();
+
+        return $logFiles;
+    }
 
 }
