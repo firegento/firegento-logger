7908471dcd15899705c8c1d462def144071cdcdc
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2015-05-30 17:05:40 +0200

    add logging to Papertrail (based on Logglysyslog)

---
+++ src/app/code/community/FireGento/Logger/Model/Papertrailsyslog.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * This file is part of a FireGento e.V. module.
+ *
+ * This FireGento e.V. module is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This script is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * PHP version 5
+ *
+ * @category  FireGento
+ * @package   FireGento_Logger
+ * @author    FireGento Team <team@firegento.com>
+ * @copyright 2013 FireGento Team (http://www.firegento.com)
+ * @license   http://opensource.org/licenses/gpl-3.0 GNU General Public License, version 3 (GPLv3)
+ */
+require_once 'rsyslog/rsyslog.php';
+/**
+ * Model for Papertrail Syslog logging
+ *
+ * @category FireGento
+ * @package  FireGento_Logger
+ * @author   FireGento Team <team@firegento.com>
+ */
+class FireGento_Logger_Model_Papertrailsyslog extends FireGento_Logger_Model_Rsyslog
+{
+
+    /**
+     * Class constructor
+     *
+     * @param  string $filename Filename
+     * @return FireGento_Logger_Model_Papertrailsyslog
+     */
+    public function __construct($filename)
+    {
+        /* @var $helper FireGento_Logger_Helper_Data */
+        $helper = Mage::helper('firegento_logger');
+        $this->_options['AppName'] = $helper->getLoggerConfig('papertrailsyslog/app_name');
+        $this->_hostName = $helper->getLoggerConfig('papertrailsyslog/hostname');
+        $this->_port = $helper->getLoggerConfig('papertrailsyslog/port');
+        return $this;
+    }
+
+    /**
+     * Builds a Message that will be sent to the Papertrail Server.
+     *
+     * @param  FireGento_Logger_Model_Event $event A Magento Log Event.
+     * @return string A string representing the message.
+     */
+    protected function buildSysLogMessage($event)
+    {
+        return new FireGento_Logger_Model_Papertrail_PapertrailSyslogMessage(
+            $this->_formatter->format($event, $this->_enableBacktrace),
+            $this->_options['AppName'],
+            $event->getPriority(),
+            strtotime($event->getTimestamp())
+        );
+    }
+}
