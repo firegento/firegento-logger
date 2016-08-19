4efccd1f4790eda99f98a038338ea3a38a32ebc5
Colin Mollenhour <colin@mollenhour.com>
2016-08-19 15:06:07 -0400

    Convert tables to InnoDb for compatibility with Galera.

---
+++ src/app/code/community/FireGento/Logger/sql/logger_setup/mysql4-upgrade-1.3.0-1.3.1.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Magpleasure Ltd.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the EULA
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.magpleasure.com/LICENSE.txt
+ *
+ * @category   Magpleasure
+ * @package    Magpleasure_Adminlogger
+ * @copyright  Copyright (c) 2012 Magpleasure Ltd. (http://www.magpleasure.com)
+ * @license    http://www.magpleasure.com/LICENSE.txt
+ */
+
+$this->run("ALTER TABLE {$this->getTable('advanced_logger')} ENGINE = InnoDb;");
