0afdd2dc850ae86b02a3af58dd69151b6e242c8d
Paul Hachmang <paul@h-o.nl>
2016-10-25 13:50:04 +0200

    Better formatting for PapertrailSyslogMessage

--- src/app/code/community/FireGento/Logger/Model/Papertrail/PapertrailSyslogMessage.php
+++ src/app/code/community/FireGento/Logger/Model/Papertrail/PapertrailSyslogMessage.php
@@ -18,6 +18,7 @@
  * @copyright 2013 FireGento Team (http://www.firegento.com)
  * @license   http://opensource.org/licenses/gpl-3.0 GNU General Public License, version 3 (GPLv3)
  */
+
 /**
  * Implementation of Remote Syslog Message for Papertrail.
  *
@@ -27,25 +28,50 @@
  */
 class FireGento_Logger_Model_Papertrail_PapertrailSyslogMessage extends SyslogMessage
 {
+    public function GetMessageChunks()
+    {
+        $MessageTag = trim($this->GetTag());
+        $MessageText = trim($this->FormatMessage());
 
-    /**
-    * Puts all Log Message elements together to form a string that will be passed
-    * to the Papertrail Server.
-    *
-    * @return string The Message as a string.
-    */
-    protected function FormatMessage() {
-        return sprintf('%s %s',
-            $this->Facility,
-            $this->Message
-        );
+        return [sprintf('%s:%s', $MessageTag, $MessageText)];
     }
 
     /**
-     * @return array
+     * Builds the TAG part of the Syslog Message.
+     *
+     * @return string The TAG part of the Syslog Message, as specified by RFC3164.
      */
-    public function GetMessageChunks()
+    private function GetTag()
     {
-        return array($this->FormatMessage());
+        $Tag = sprintf('%s%s %s ', $this->GetPriority(),
+            $this->GetSyslogTimestamp(),
+            $this->GetHostName());
+        return substr($Tag, 0, self::MAX_TAG_LENGTH);
+    }
+
+    /**
+     * Calculates and returns the Priority of a Log Message.
+     *
+     * @return string A value indicating the Priority of the Log Message.
+     */
+    private function GetPriority()
+    {
+        return sprintf('<%d>', $this->Facility * 8 + $this->Severity);
+    }
+
+
+    /**
+     * Returns a Timestamp string, formatted as required by RFC3164.
+     *
+     * @return string A Timestamp string, formatted as required by RFC3164.
+     */
+    private function GetSyslogTimestamp()
+    {
+        // It's a bit messy to format the timestamp as required by RFC3164, because
+        // the Day has to be padded with spaces and date() doesn't allow it. For this
+        // reason, the Timestamp is first formatted as "Month %2s Hours:Minutes:Seconds",
+        // then this string is used with sprintf to pad the day returned by the second
+        // date() statement.
+        return sprintf(date('M %2\s H:i:s', $this->Timestamp), date('j', $this->Timestamp));
     }
 }
